6d6e6d2baab39e95e5533e5e32ca67f50ebc81a5
r.pool <r.pool@f67a8107-f8d1-4425-a0ae-94eea21ecd1d>
2012-11-14 10:38:39 +0000

    git-svn-id: svn+ssh://clio.psy.vu.nl/home/r.pool/svn/BioCrates/trunk@152 f67a8107-f8d1-4425-a0ae-94eea21ecd1d

--- MetaAnalysis/Manhattan3DPlot/Manhattan3DPlot.py
+++ MetaAnalysis/Manhattan3DPlot/Manhattan3DPlot.py
@@ -571,8 +571,6 @@ def main(ExecutableName):
         print len(XTicks)
         PylabAxis.w_xaxis.set_ticklabels(XTickLabels)
         PylabAxis.w_xaxis.set_major_locator(ticker.FixedLocator(XTicks))
-        print PylabAxis.w_xaxis.get_major_locator()
-        print len(PylabAxis.w_xaxis.get_major_locator())
 
 
 #        PylabAxis.set_zlim3d([0.0,PylabAxis.get_zlim3d()[-1]])
